4d63d780640dfd6717a24b7511e0615da1b085ea
Caleb Fletcher <caleb@fletcher.cf>
2025-10-09 21:14:07 +1100

    feat: add immediate extraction to instruction decoding

--- targets/Arty/hdl/InstructionDecoder.vhd
+++ targets/Arty/hdl/InstructionDecoder.vhd
@@ -20,15 +20,32 @@ BEGIN
 
     PROCESS (ALL)
     BEGIN
+        -- extract immediate from instruction
         CASE instructionEncoding IS
             WHEN R =>
                 immediate <= (OTHERS => '0');
             WHEN I =>
-                immediate <= STD_LOGIC_VECTOR(resize(signed(instruction(31 DOWNTO 20)), 32));
+                immediate(31 DOWNTO 11) <= (OTHERS => instruction(31));
+                immediate(10 DOWNTO 0) <= instruction(30 DOWNTO 20);
             WHEN S =>
-                immediate <= STD_LOGIC_VECTOR(resize(signed(instruction(31 DOWNTO 25) & instruction(11 DOWNTO 7)), 32));
-            WHEN OTHERS =>
-                NULL;
+                immediate(31 DOWNTO 11) <= (OTHERS => instruction(31));
+                immediate(10 DOWNTO 5) <= instruction(30 DOWNTO 25);
+                immediate(4 DOWNTO 0) <= instruction(11 DOWNTO 7);
+            WHEN B =>
+                immediate(31 DOWNTO 12) <= (OTHERS => instruction(31));
+                immediate(11) <= instruction(7);
+                immediate(10 DOWNTO 5) <= instruction(30 DOWNTO 25);
+                immediate(4 DOWNTO 1) <= instruction(11 DOWNTO 8);
+                immediate(0) <= '0';
+            WHEN U =>
+                immediate(31 DOWNTO 12) <= instruction(31 DOWNTO 12);
+                immediate(11 DOWNTO 0) <= "000000000000";
+            WHEN J =>
+                immediate(31 DOWNTO 20) <= (OTHERS => instruction(31));
+                immediate(19 DOWNTO 12) <= instruction(19 DOWNTO 12);
+                immediate(11) <= instruction(20);
+                immediate(10 DOWNTO 1) <= instruction(30 DOWNTO 21);
+                immediate(0) <= '0';
         END CASE;
     END PROCESS;
 
