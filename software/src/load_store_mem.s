222ab39741c7f8228ace837514aa3c98f02b6e40
Caleb Fletcher <caleb@fletcher.cf>
2025-10-13 23:24:10 +1100

    feat: add linker script to sw assembler

--- software/src/load_store_mem.s
+++ software/src/load_store_mem.s
@@ -2,9 +2,14 @@
     .globl _start
 
 _start:
-    la x1, 0x12345678
+    la x1, val1
     lw x2, 0(x1)
-    li x3, 0xdeadbeef
+    la x3, val2
     sw x2, 0(x3)
 
     ebreak
+
+    .section .data
+val1: .word 0x12345678
+val2: .word 0xdeadbeef
+val3: .word 0x01010202
