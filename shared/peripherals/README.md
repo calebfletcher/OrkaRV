0c4f600ecd6345a9588b8659fb31a4e163b5b93a
Caleb Fletcher <caleb@fletcher.cf>
2025-12-02 22:26:00 +1100

    docs: add docs for peripherals

---
+++ shared/peripherals/README.md
@@ -0,0 +1,5 @@
+# OrkaRV Peripherals
+
+| Peripheral | Description           | Registers                      |
+| ---------- | --------------------- | ------------------------------ |
+| GPIO       | Basic GPIO peripheral | [Registers](gpio/Registers.md) |
--- software/runner.py
+++ software/runner.py
@@ -34,9 +34,9 @@ def main():
         proj_path / "shared" / "hdl" / "axi4lite_intf_pkg.vhd",
         proj_path / "shared" / "hdl" / "AxiLitePeakRdlBridge.vhd",
         
-        proj_path / "shared" / "gpio" / "hdl" / "GpioRegisters_pkg.vhd",
-        proj_path / "shared" / "gpio" / "hdl" / "GpioRegisters.vhd",
-        proj_path / "shared" / "gpio" / "hdl" / "Gpio.vhd",
+        proj_path / "shared" / "peripherals" / "gpio" / "hdl" / "GpioRegisters_pkg.vhd",
+        proj_path / "shared" / "peripherals" / "gpio" / "hdl" / "GpioRegisters.vhd",
+        proj_path / "shared" / "peripherals" / "gpio" / "hdl" / "Gpio.vhd",
 
         proj_path / "shared" / "hdl" / "Soc.vhd",
     ]
